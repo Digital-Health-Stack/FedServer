05e9a1e9a83127e22fb28513de7dfdfbed1ac63b
Prashu7487 <pm7487912@gmail.com>
2025-03-21 14:31:50 +0530

    merged next-version after resolving conflict

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -9,38 +9,11 @@ import { Route, Routes } from "react-router-dom";
 export default function App() {
   return (
     <>
-<<<<<<< HEAD
-        <NavBar />
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            margin: "100px",
-          }}
-        >
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-
-            <Route path="/Results" element={<TrainingResults />} />
-
-            <Route
-              path="/TrainingResults/details/:sessionId"
-              element={<ResultDetails />}
-            />
-
-            <Route path="/About" element={<About />} />
-
-            <Route path="/*" element={<Error />} />
-          </Routes>
-        </div>
-=======
       <NavBar />
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="*" element={<Error />} />
       </Routes>
->>>>>>> next-version
     </>
   );
 }
--- frontend/app/src/Pages/Home.jsx
+++ frontend/app/src/Pages/Home.jsx
@@ -8,40 +8,6 @@ export default function Home() {
   };
 
   return (
-<<<<<<< HEAD
-    <div className="container-fluid d-flex align-items-center">
-      <div className="row w-100">
-        <div className="col-md-5 d-none d-md-block">
-          <img src={StepsGif} alt="Steps" className="img-fluid" />
-        </div>
-        <div className="col-md-7 d-flex justify-content-center align-items-center">
-          <div className="card shadow-md mb-5 rounded">
-            <div className="card-body">
-              <div className="text-center mb-4">
-                <img
-                  src={FedClientImage}
-                  className="rounded-circle"
-                  alt="FedClient"
-                  width="150"
-                  height="150"
-                />
-              </div>
-              <h1 className="text-center mb-4 text-dark">
-                Welcome to FedClient
-              </h1>
-              <div className="alert alert-info text-center" role="alert">
-                <strong>
-                  This is Client Application to simulate Federated Learning
-                </strong>
-              </div>
-              <button className="btn btn-success w-100" onClick={gotoRegister}>
-                Get Started
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-=======
     <div className="flex flex-col items-center">
       <h1 className="text-3xl font-bold underline text-center text-red-500">
         Click on the Vite and React logos to learn more code
@@ -52,7 +18,6 @@ export default function Home() {
       >
         Register
       </button>
->>>>>>> next-version
     </div>
   );
 }
