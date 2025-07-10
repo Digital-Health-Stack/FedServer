5ff86bfa66aa6e4d391c8016cb51ad9cfae0b4c6
Keshav76 <keshav.banka2003@gmail.com>
2025-07-10 10:26:32 +0530

    Move redis out of start script

--- start_local.sh
+++ start_local.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-redis-server backend/app/redis.conf &
 concurrently -n BACKEND,FRONTEND -c yellow,cyan \
 "cd backend/app && . venv/bin/activate && uvicorn main:app --host 0.0.0.0 --port 8000 --reload" \
 "cd frontend/app && npm run dev"
---
+++ start_redis.sh
@@ -0,0 +1,1 @@
+redis-server backend/app/redis.conf &
