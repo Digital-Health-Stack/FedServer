abbac37b7474666e8867ee7daa027edf8d8d4e7c
harshbaid-13 <harshbaid.ind@gmail.com>
2026-05-26 13:10:03 +0530

    enable unbuffered output

--- start_local.sh
+++ start_local.sh
@@ -2,5 +2,5 @@
 redis-server backend/utilities/redis.conf &
 
 concurrently -n BACKEND,FRONTEND -c yellow,cyan \
-"cd backend && . venv/bin/activate && uvicorn main:app --host 0.0.0.0 --port 8000 --reload" \
+"cd backend && . venv/bin/activate && PYTHONUNBUFFERED=1 uvicorn main:app --host 0.0.0.0 --port 8000 --reload" \
 "cd frontend && npm run dev"
